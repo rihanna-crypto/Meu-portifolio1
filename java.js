8cc5b1e392a8def01f48d2eb11832425e43bbfb0
rihanna-crypto <rrihanna.nuness@gmail.com>
2026-06-05 09:33:51 -0300

    Add files via upload

---
+++ java.js
@@ -0,0 +1,35 @@
+'use strict'
+
+function getProjects(){
+    const urlGithub = 'https://api.github.com/users/rihanna-crypto/repos'
+    var loadingElement = document.getElementById('loading')
+    fetch(urlGithub, {
+        method : 'GET'
+    })
+        .then((response) => response.json())
+        .then((response) => {
+            loadingElement.style.display = 'none'
+            showProjects(response)
+        })
+
+    .catch((e) => {
+        console.log(e)
+    })
+}
+
+function showProjects(data){
+    var listElement = document.getElementById('lista-meus-projetos')
+    for(let i = 0; i < data.length; i++){
+        let a = document.createElement('a')
+        a.href = data[i]['clone_url']
+        a.target = '_blank'
+        a.title = data[i]['description']
+        let linkText = document.createTextNode(data[i]['name'])
+        a.appendChild(linkText)
+        listElement.appendChild(a)
+    }
+
+}
+
+getProjects()
+
